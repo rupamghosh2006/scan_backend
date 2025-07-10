c61329ff07773d5c9ea2031b2cfe0f5114387d21
rupamghosh2006 <rupamgh2006@gmail.com>
2025-07-10 12:32:41 +0530

    created data model of question bank

---
+++ src/models/question.models.js
@@ -0,0 +1,33 @@
+import mongoose, {Schema} from "mongoose";
+
+const questionSchema = new Schema({
+    chapter: {
+        type: String,
+        required: true,
+    },
+    class: {
+        type: Number,
+        required: true,
+    },
+    correctAnswer: {
+        type: String,
+        required: true,
+    },
+    options: {
+        A: { type: String, required: true },
+        B: { type: String, required: true },
+        C: { type: String, required: true },
+        D: { type: String, required: true },
+    },
+    question: {
+        type: String,
+        required: true,
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+},
+    {timestamps: true});
+
+export const Question = mongoose.model("Question", questionSchema);    
